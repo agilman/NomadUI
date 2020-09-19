17e7f0b60d8176e0c58f6c4a8b625338eca59861
agilman <gilmanalex@gmail.com>
2020-09-19 12:34:01 -0700

    Displaying links to login and registration pages when not logged in.

--- layouts/viewer.vue
+++ layouts/viewer.vue
@@ -9,32 +9,46 @@
         </nuxt-link>
       </div>
       <!-- AUTH -->
-      <div class="flex flex-wrap items-center p-2 items-center">
-        <div class="text-teal-200 hover:text-white">
-          <span> Logged in as [user]</span>
+      <span v-if="loggedIn">
+        <div class="flex flex-wrap items-center p-2 items-center">
+          <div class="text-teal-200 hover:text-white">
+            <span> Logged in as [user]</span>
+          </div>
+          <button id="nav-toggle" class="flex items-center px-3 py-2 border-2 rounded border-black hover:text-white hover:border-white">
+            <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
         </div>
-        <button id="nav-toggle" class="flex items-center px-3 py-2 border-2 rounded border-black hover:text-white hover:border-white">
-          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-      </div>
-      <div id="nav-content" class="w-0 flex-shrink hidden sm:block pt-6 lg:pt-0">
-        <ul class="list-reset lg:flex justify-end flex-1 items-center">
-          <li class="mr-3">
-            <nuxt-link to="/auth/logout" class="flex py-2 px-4 text-white no-underline">
-              Log out
+        <div id="nav-content" class="w-0 flex-shrink hidden sm:block pt-6 lg:pt-0">
+          <ul class="list-reset lg:flex justify-end flex-1 items-center">
+            <li class="mr-3">
+              <nuxt-link to="/auth/logout" class="flex py-2 px-4 text-white no-underline">
+                Log out
+              </nuxt-link>
+            </li>
+            <li class="mr-3">
+              <a class="flex py-2 px-4 text-white no-underline" href="#">link</a>
+            </li>
+            <li class="mr-3">
+              <a class="flex py-2 px-4 text-white no-underline" href="#">link</a>
+            </li>
+          </ul>
+        </div>
+      </span>
+      <span v-else>
+        <div class="flex flex-wrap items-center p-2 items-center">
+          <div class="text-teal-200" >
+            <nuxt-link to="/auth/login" class="hover:text-white">
+              Log In
+            </nuxt-link>|
+            <nuxt-link to="/auth/register" class="hover:text-white">
+              Register
             </nuxt-link>
-          </li>
-          <li class="mr-3">
-            <a class="flex py-2 px-4 text-white no-underline" href="#">link</a>
-          </li>
-          <li class="mr-3">
-            <a class="flex py-2 px-4 text-white no-underline" href="#">link</a>
-          </li>
-        </ul>
-      </div>
+          </div>
+        </div>
+      </span>
     </nav>
     <!-- END NAV BAR -->
 
